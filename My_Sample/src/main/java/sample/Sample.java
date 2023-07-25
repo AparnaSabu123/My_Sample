a9970fcc29bb2e6cc38c0d15547374b5fef30e8c
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-07-25 09:00:57 +0530

    first commit

---
+++ My_Sample/src/main/java/sample/Sample.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=9;
+		System.out.println(a+b);
+		// TODO Auto-generated method stub
+
+	}
+
+}
